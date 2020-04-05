dd1812adc7060420f88de4ab82fa7ffe647fe317
rckey <private.richardk@gmail.com>
2020-04-05 02:09:43 +0200

    remove duplicates

--- get-node-info/src/main.rs
+++
@@ -1,8 +0,0 @@
-use iota_lib_rs::prelude::*;
-
-fn main() {
-    let mut api = iota_client::Client::new("https://nodes.devnet.iota.org:443");
-    let node_info = api.get_node_info().unwrap();
-
-    println!("{:?}", node_info);
-}
