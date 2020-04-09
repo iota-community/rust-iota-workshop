9c067646a46d80b64bc838742a2d68d1237a1c5d
rckey <private.richardk@gmail.com>
2020-04-09 20:10:52 +0200

    refactor

--- i-streams-publish/src/main.rs
+++ i-streams-publish/src/main.rs
@@ -1,13 +1,15 @@
-use failure::{Fallible};
-use iota_streams_app::transport::Transport;
-use iota_streams_app_channel::{
-    api::tangle::{Author}
-};
+use failure::Fallible;
+use iota_streams::app::transport::Transport;
+use iota_streams::app_channel::api::tangle::Author;
 
 fn example() -> Fallible<()> {
     let mut client = iota_lib_rs::iota_client::Client::new("https://nodes.devnet.iota.org:443");
 
-    let mut author = Author::new("AGDXXACSRTFNNJAFT9TNAABRUHRKUEHZALMUVQ9Z9UNO9VCGN9NJIYVFFXQ9QHEKSN9DNEVGFGYQOB9MV", 2, false);
+    let mut author = Author::new(
+        "AGDXXACSRTFNNJAFT9TNAABRUHRKUEHZALMUVQ9Z9UNO9VCGN9NJIYVFFXQ9QHEKSN9DNEVGFGYQOB9MV",
+        2,
+        false,
+    );
 
     println!("announce");
     let announcement = author.announce()?;
