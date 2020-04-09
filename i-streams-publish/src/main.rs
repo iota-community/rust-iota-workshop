23d1ead4148eb5eae72d865465c6e1f6a76d5095
huhn511 <sehe89@gmail.com>
2020-04-09 18:44:48 +0200

    remove unused imports

--- i-streams-publish/src/main.rs
+++ i-streams-publish/src/main.rs
@@ -1,19 +1,13 @@
-use failure::{ensure, Fallible};
-use iota_streams_app::message::HasLink;
+use failure::{Fallible};
 use iota_streams_app::transport::Transport;
 use iota_streams_app_channel::{
-    api::tangle::{Author, Subscriber},
-    message,
+    api::tangle::{Author}
 };
-use iota_streams_core::tbits::Tbits;
-use iota_streams_protobuf3::types::Trytes;
-use std::str::FromStr;
 
 fn example() -> Fallible<()> {
     let mut client = iota_lib_rs::iota_client::Client::new("https://nodes.devnet.iota.org:443");
 
     let mut author = Author::new("AGDXXACSRTFNNJAFT9TNAABRUHRKUEHZALMUVQ9Z9UNO9VCGN9NJIYVFFXQ9QHEKSN9DNEVGFGYQOB9MV", 2, false);
-    let mut subscriber = Subscriber::new("SGDXXACSRTFNNJAFT9TNAABRUHRKUEHZALMUVQ9Z9UNO9VCGN9NJIYVFFXQ9QHEKSN9DNEVGFGYQOB9MA", false);
 
     println!("announce");
     let announcement = author.announce()?;
