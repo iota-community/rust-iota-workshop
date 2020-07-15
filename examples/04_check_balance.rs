ccb2ae4c2a756962d1a0ce44d0df3743202dc4a0
huhn511 <huhn511@users.noreply.github.com>
2020-07-15 16:34:41 +0000

    deploy: 535a7f7962af02fd08032076b68c65fc5f319469

--- examples/04_check_balance.rs
+++ examples/04_check_balance.rs
@@ -6,9 +6,25 @@
 //! cargo run --example 04_check_balance
 //! ```
 use anyhow::Result;
+use iota::{
+    bundle::{Address, TransactionField},
+    ternary::TryteBuf,
+};
 
 #[smol_potat::main]
 async fn main() -> Result<()> {
-    println!("Not implemented yet!");
+    iota::Client::add_node("https://nodes.comnet.thetangle.org")?;
+    let response = iota::Client::get_balances()
+        .addresses(&[Address::from_inner_unchecked(
+            TryteBuf::try_from_str(
+                "LOLCUVZ9MBPBTGJGMRLHNLDGCNAWUCGNRBBKUGKRAUSGRCHYXZPGEBXBPJFTBPYPNMCYNDCFZTFYSCXEBLDPKQBUJ9",
+            )
+            .unwrap()
+            .as_trits()
+            .encode(),
+        )])
+        .send()
+        .await?;
+    println!("Balance: {:#?}", response.balances[0]);
     Ok(())
 }
