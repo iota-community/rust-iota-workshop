81501d4b0a73fdcfd5b9e529f295b34361c76951
huhn511 <sehe89@gmail.com>
2020-12-18 21:50:26 +0100

    add 00_get_node_info

--- examples/00_get_node_info.rs
+++ examples/00_get_node_info.rs
@@ -5,14 +5,18 @@
 //! ```
 //! cargo run --example 00_get_node_info
 //! ```
-use anyhow::Result;
 
-#[smol_potat::main]
-async fn main() -> Result<()> {
-    const DEFAULT_NODE_URL: &str = "https://api.lb-0.testnet.chrysalis2.com";
+use iota::{Client};
 
-    let node_info = iota_client::Client::get_node_info(DEFAULT_NODE_URL).await.unwrap();
+#[tokio::main]
+async fn main() {
+
+    let iota = Client::builder() // Crate a client instance builder
+    .node("https://api.lb-0.testnet.chrysalis2.com") // Insert the node here
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let node_info = iota.get_info().await.unwrap();
     println!("{:#?}", node_info);
-
-    Ok(())
 }
