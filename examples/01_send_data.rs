08d7c62a0862bf0a03dd6c79d7c98f20eb03716a
huhn511 <sehe89@gmail.com>
2020-12-18 22:09:28 +0100

    add send and fetch data

--- examples/01_send_data.rs
+++ examples/01_send_data.rs
@@ -5,56 +5,24 @@
 //! ```
 //! cargo run --example 01_send_data
 //! ```
-use anyhow::Result;
-use iota::{
-    client::Transfer,
-    ternary::TryteBuf,
-    transaction::bundled::{Address, BundledTransactionField},
-};
-use iota_conversion::Trinary;
 
-#[smol_potat::main]
-async fn main() -> Result<()> {
-    // Prepare a vector of transfers
-    let mut transfers = Vec::new();
+use iota::Client;
 
-    // Push the transfer to vector.
-    transfers.push(Transfer {
-        // Address is 81 trytes.
-        address: Address::from_inner_unchecked(
-            TryteBuf::try_from_str(
-                "ADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDR",
-            )
-            .unwrap()
-            .as_trits()
-            .encode(),
-        ),
-        // We are using a zero balance seed so we make a zero value transfer here
-        value: 0,
-        message: Some(String::from("Hello Tangle!")),
-        tag: None,
-    });
+#[tokio::main]
+async fn main() {
+    let iota = Client::builder() // Crate a client instance builder
+        .node("https://api.lb-0.testnet.chrysalis2.com") // Insert the node here
+        .unwrap()
+        .build()
+        .unwrap();
 
-    // Create a client instance
-    let iota = iota::ClientBuilder::new()
-        .node("https://nodes.comnet.thetangle.org")?
-        .build()?;
-    // Call send_transfers api
-    let res = iota
-        .send(None)
-        // Input the transfers
-        .transfers(transfers)
-        // We are sending to comnet, so mwm should be 10. It's 14 by default if you don't call this.
-        .min_weight_magnitude(10)
-        // Sending to the node and receive the response
+    let response = iota
         .send()
-        .await?;
+        .indexation("Hello Tangle".to_string())
+        .data("Hello World!".to_string().as_bytes().to_vec())
+        .post()
+        .await
+        .unwrap();
 
-    // The response of send_transfers is vector of Transaction type. We choose the first one and see what is its bundle hash
-    println!(
-        "Search in theTangle: https://comnet.thetangle.org/bundle/{}",
-        res[0].bundle().to_inner().as_i8_slice().trytes().unwrap()
-    );
-
-    Ok(())
+    println!("MessageId {}", response);
 }
--- examples/02_fetch_data.rs
+++ examples/02_fetch_data.rs
@@ -5,57 +5,24 @@
 //! ```
 //! cargo run --example 02_fetch_data
 //! ```
-use anyhow::Result;
-use iota::{
-    ternary::TryteBuf,
-    transaction::bundled::{Address, BundledTransactionField},
-};
-use iota_conversion::{trytes_converter, Trinary};
+use iota::{Client, Payload};
 
-#[smol_potat::main]
-async fn main() -> Result<()> {
-    let iota = iota::ClientBuilder::new()
-        .node("https://nodes.comnet.thetangle.org")?
-        .build()?;
-    let address = Address::from_inner_unchecked(
-        TryteBuf::try_from_str(
-            "ADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDRESSADDR",
-        )
+#[tokio::main]
+async fn main() {
+    let iota = Client::builder() // Crate a client instance builder
+        .node("https://api.lb-0.testnet.chrysalis2.com") // Insert the node here
         .unwrap()
-        .as_trits()
-        .encode(),
-    );
+        .build()
+        .unwrap();
 
-    let response = iota
-        .find_transactions()
-        .addresses(&[address])
-        .send()
-        .await?;
+    let fetched_messages = iota.get_message().index(&"Hello Tangle").await.unwrap();
 
-    let transactions = iota.get_bundle(&response.hashes[0]).await?;
+    let last_message = iota.get_message().data(&fetched_messages[fetched_messages.len() - 1]).await.unwrap();
 
-    let trytes_coll: Vec<String> = transactions
-        .iter()
-        .map(|t| {
-            t.payload()
-                .to_inner()
-                .as_i8_slice()
-                .trytes()
-                .unwrap()
-                .trim_end_matches('9')
-                .to_string()
-        })
-        .collect();
-
-    let message = match trytes_converter::to_string(&trytes_coll.concat()) {
-        Ok(m) => m,
-        Err(e) => {
-            println!("Error: trytes_converter.to_string()\n\t{}", e);
-            std::process::exit(1);
-        }
-    };
-
-    println!("Message:\n{}", message);
-
-    Ok(())
+    if let Payload::Indexation(i) = last_message.payload().as_ref().unwrap() {
+        println!(
+            "Data: {}",
+            String::from_utf8(i.data().to_vec()).expect("Found invalid UTF-8")
+        );
+    }
 }
